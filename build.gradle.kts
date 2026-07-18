0f22d2862bcf56ea0378a8e271daef95e5ba7dd9
sara1439786 <sara1439@gmail.com>
2026-07-19 00:32:23 +0530

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,7 @@
-/build
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+}
